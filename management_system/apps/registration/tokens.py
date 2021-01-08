ce7f003fcea1e8dac700fd54bd0eccc24f3eba07
achopik <a-chopik@mail.ru>
2021-01-08 20:26:04 +0300

    Implemented registration logic

---
+++ management_system/apps/registration/tokens.py
@@ -0,0 +1,43 @@
+from typing import Union
+
+from django.contrib.auth.models import User
+
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import AccessToken
+
+
+class TokenWorker:
+    """
+    Class providing static methods for generation and confirmation of JWT
+    """
+
+    @staticmethod
+    def check_token(token) -> Union[int, bool]:
+        """
+        Returns user id and confirms user if token is valid and user exists
+        """
+
+        uid = _get_user_id(token)
+        user = User.objects.get(id=uid)
+        if uid and user:
+            user.profile.is_confirmed = True
+            user.profile.save()
+            return uid
+        return False
+
+    @staticmethod
+    def create_token(user) -> str:
+        """
+        Generates token for a certain user
+        """
+        token = AccessToken.for_user(user)
+        return str(token)
+
+    @staticmethod
+    def _get_user_id(token) -> Union[int, bool]:
+        try:
+            user_id = AccessToken(token, verify=True).get('user_id')
+            return user_id
+
+        except TokenError:
+            return False
