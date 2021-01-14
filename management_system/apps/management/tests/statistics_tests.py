e4de5d5463212f06dd8619e1d3d2615ef40c7c41
achopik <a-chopik@mail.ru>
2021-01-14 11:31:47 +0300

    Statistics endpoints now are action-based

--- management_system/apps/management/tests/statistics_tests.py
+++ management_system/apps/management/tests/statistics_tests.py
@@ -16,49 +16,6 @@ from management.services.statistics import (
 pytestmark = pytest.mark.django_db(transaction=True, reset_sequences=True)
 
 
-class DepartmentStatsTest(APITestCase):
-
-    url = reverse("department-statistics-detail", args=["1"])
-
-    @pytest.mark.usefixtures("department_obj")
-    def test_department_statistics_endpoint_available(self):
-        """
-        Checks if we can use department statistics endpoint without errors
-        """
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    @pytest.mark.usefixtures("department_obj")
-    def test_department_statistics_endpoint_send_data(self):
-        """
-        Checks if endpoint department statistics sends data
-        """
-        response = self.client.get(self.url)
-        self.assertIn("statistics", response.data)
-        self.assertIn("total_employees", response.data["statistics"])
-
-
-class OpportunityStatsTest(APITestCase):
-
-    url = reverse("opportunity-statistics-list")
-
-    @pytest.mark.usefixtures("opportunity_obj")
-    def test_opportunity_statistics_endpoint_available(self):
-        """
-        Checks if we can use opportunity statistics endpoint without errors
-        """
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    @pytest.mark.usefixtures("opportunity_obj")
-    def test_opportunity_statistics_endpoint_send_data(self):
-        """
-        Checks if endpoint opportunity statistics sends data
-        """
-        response = self.client.get(self.url)
-        self.assertIn("statistics", response.data)
-
-
 class DomainStatsTest(APITestCase):
 
     url = "domain-statistics-detail"
